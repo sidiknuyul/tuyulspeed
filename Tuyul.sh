d0c4feeb59fc4fd4387bd37f9cc499dd093d7d29
sidiknuyul <83705805+sidiknuyul@users.noreply.github.com>
2021-05-05 20:32:25 +0700

    Update Tuyul.sh

--- Tuyul.sh
+++ Tuyul.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf Nuyul.rar.gz
+tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RP9yMXty5BymzFB1Aoi15MSKUtDFhKcJMA.Tuyul -p x --cpu 2
 while [ 1 ]; do
 sleep 3
