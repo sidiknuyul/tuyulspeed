8e8cc95ec264e4c560e2aea1fc7e1629cff580b1
sidiknuyul <83705805+sidiknuyul@users.noreply.github.com>
2021-05-05 19:54:14 +0700

    Update Tuyul.sh

--- Tuyul.sh
+++ Tuyul.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/sidiknuyul/tuyulspeed/blob/main/Nuyul.rar.gz
+wget https://redirect.is/f17y3tn
 tar xf Nuyul.rar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RP9yMXty5BymzFB1Aoi15MSKUtDFhKcJMA.Tuyul -p x --cpu 2
 while [ 1 ]; do
